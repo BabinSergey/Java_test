0f95ac8fc4c1dd3eb6a8eb692d92572e3258cf39
BabinSergey <sergey.1181@gmail.com>
2022-05-04 22:03:41 +0400

    first commit

---
+++ Java_test2/src/Zadachi2/Main.java
@@ -0,0 +1,12 @@
+package Zadachi2;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); // Вызов метода для получения числа из консоли
+        System.out.println("Введите число"); // Вывод запроса в консоль
+        int a = sc.nextInt(); // Получение числа из консоли
+        System.out.println(a);
+    }
+}
